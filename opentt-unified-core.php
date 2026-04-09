47dfe76d400b0811e7da74c71616c0eb0c44f478
Aleksa Dimitrijević <tradicije@icloud.com>
2026-04-09 13:20:42 +0200

    Revert "Varoius fixes"
    This reverts commit 5d855651ca87aadcece3771f2307d7ef09e780c3.

---
+++ opentt-unified-core.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * OpenTT - Table Tennis Management Plugin
+ * Copyright (C) 2026 Aleksa Dimitrijević
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ */
+
+/**
+ * Plugin Name: OpenTT
+ * Description: OpenTT table tennis management plugin for competitions, clubs, players, and match data.
+ * Version: 1.1.0
+ * Author: Aleksa Dimitrijević
+ * Author URI: https://instagram.com/tradicije
+ * License: AGPL-3.0-or-later
+ * License URI: https://www.gnu.org/licenses/agpl-3.0.html
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+$autoload = __DIR__ . '/vendor/autoload.php';
+if (file_exists($autoload)) {
+    require_once $autoload;
+}
+
+if (!class_exists('OpenTT\\Unified\\Plugin')) {
+    require_once __DIR__ . '/src/Plugin.php';
+}
+
+\OpenTT\Unified\Plugin::boot(__FILE__);
